e7625cf1fbec953e177645dbed050042438ce44a
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-12-03 12:36:17 +0800

    Update Index.js

--- 01__POMODORO/STARTER-FILES/Index.js
+++ 01__POMODORO/STARTER-FILES/Index.js
@@ -3,3 +3,5 @@
 // - Once the user clicks start, the word start will change to stop. Then, the user can click on the stop button to make the timer stop.
 // - Click on the gear icon to change the length (minutes and seconds) of the timer.
 // - Once the timer finishes, the ring should change from green to red and an alert message is passed to the browser.
+
+const startBtn = document.querySelector('.start')
