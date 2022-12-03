197a049542a73eeb95ce6e2506d530651f086a15
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-12-03 12:39:32 +0800

    Update Index.js

--- 01__POMODORO/STARTER-FILES/Index.js
+++ 01__POMODORO/STARTER-FILES/Index.js
@@ -4,7 +4,16 @@
 // - Click on the gear icon to change the length (minutes and seconds) of the timer.
 // - Once the timer finishes, the ring should change from green to red and an alert message is passed to the browser.
 
+
+function timer(){
+    let time = 5;
+    if (time < 0) {
+        time--
+    }
+    console.log(time)
+}
+
 const startBtn = document.querySelector('.start')
 startBtn.addEventListener('click', () =>{
-    alert("Youve clicked me")
+   timer()
 })
